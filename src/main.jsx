dcf62115505552f61bd3eeba5a596e756f3553da
developer-assets <999patrickobama@gmail.com>
2024-04-11 21:33:18 +0300

    Added other libraries, icons, started setting up routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from './Pages/HomePage/HomePage.jsx';
+import AuthPage from './Pages/AuthPage/AuthPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/auth",
+    element: <AuthPage />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )

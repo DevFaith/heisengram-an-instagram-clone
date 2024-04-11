05ac8fddab80c9f465486b5a1d95577f32970895
developer-assets <999patrickobama@gmail.com>
2024-04-11 21:53:17 +0300

    Moved assets around

--- src/Pages/AuthPage/AuthPage.jsx
+++ src/Pages/AuthPage/AuthPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import AuthImage from '../../assets/images/auth.png'
 
 const AuthPage = () => {
   return (
-    <div>AuthPage</div>
+    <div className='mx-auto max-w-7xl'>
+      <div className='flex align-middle min-h-screen h-fit justify-center px-2'>
+        <img src={AuthImage} alt="Instagram Phone Image" className='hidden md:flex' />
+        <div>
+          fhfh
+        </div>
+      </div>
+    </div>
   )
 }
 
